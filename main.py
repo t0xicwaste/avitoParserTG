e2c7a5cba4c417d164d28ef2bdadcfaa81db7e5d
t0xicwaste <forgithub969@gmail.com>
2025-12-12 09:06:53 +0300

    fix(main.py): Инициализация бота

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+import os
+
+from loguru import logger
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv("TOKEN"))
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logger.info("Telegram bot starting!")
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.error("The bot was just stopped!")
