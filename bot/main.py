be3cf7c865caf83f5c033c4e02b6fa14850d73bf
t0xicwaste <forgithub969@gmail.com>
2026-01-25 00:45:00 +0000

    fix(main.py): Исправления нахождения токена

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,27 @@
 import asyncio
 import os
+from pathlib import Path
 
-from loguru import logger
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+from handlers.start import start_router
+from loguru import logger
 
-load_dotenv()
+env_path = Path(__file__).parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
-bot = Bot(token=os.getenv("TOKEN"))
+TOKEN = os.getenv("TOKEN")
+
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
 async def main():
+    dp.include_router(start_router)
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logger.info("Telegram bot starting!")
     try:
         asyncio.run(main())
